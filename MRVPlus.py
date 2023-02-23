08b8632ab1b68e865e410df3cb81c309f0244397
kakashi <phenominalcoder@gmail.com>
2023-02-23 09:06:01 +0530

    update

--- MRVPlus.py
+++ MRVPlus.py
@@ -2,31 +2,18 @@ from sudokuUtils import *
 
 calls = Calls()
 
-# checks if b is a peer cell to a
-def peers(a, b):
-    row = a[0]
-    column = a[1]
-    if row == b[0] or column == b[1] or \
-    (b[0] > row-row%3-1 and b[0] < row-row%3+3 and \
-    b[1] > column-column%3-1 and b[1] < column-column%3+3):
-        return True
-    return False
-
 # Identifies best potential cell based on contribution 
 def FogResolver(potential_cells_list, cells_candidates_list):
-    selected_cell = None
+    selected_cell = potential_cells_list[0]
     max_contribution = 0
     for potential_cell in potential_cells_list:
         for number in potential_cell[2]:
             contribution = 0
             for cell in cells_candidates_list:
-                if peers(potential_cell[0], cell[0]):
-                    contribution = contribution+len(set(potential_cell[2]).intersection(set(cell[2])))
+                contribution = contribution+len(set(potential_cell[2]).intersection(set(cell[2])))
             if contribution > max_contribution:
                 max_contribution = contribution
                 selected_cell = potential_cell
-    if selected_cell==None:
-        print(potential_cells_list)
     return selected_cell
 
 def solver(matrix):
@@ -73,5 +60,3 @@ def solver(matrix):
             return True
         matrix[row][column] = 0
     return False
-
-print(peers([0,3],[1,2]))
--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,8 @@
 from sudokuUtils import getMatrix
-# from Naive import solver
+# Uncomment any one import line based on your choice of algorithm
+from Naive import solver
 # from MRV import solver
-from MRVPlus import solver
+# from MRVPlus import solver
 
 matrix = getMatrix(input("File path:"))
 solver(matrix)
